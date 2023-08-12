369344c75f7a3b19aa845b5d1940701ec243f36c
Denys Novosad <denisnovosad3@gmail.com>
2023-08-12 15:16:24 +0300

    9 back

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const corsParams={
 //app.use(cors(corsParams))
 const PORT=/*config.get('SERVER_PORT')*/process.env.SERVER_PORT || 5000
 const DB_URL=process.env.DB_URL || "mongodb+srv://varolou:AF9KvMedLj90iHGd@artd.nqpcmly.mongodb.net/?retryWrites=true&w=majority" /*"mongodb+srv://varolou:5gRLitUDwPmSmOAv@cloud.upwzkiy.mongodb.net/?retryWrites=true&w=majority"*/
-app.use(cors(corsParams))
+
 app.use(express.json());
-app.use("/api/auth",authRouter);
-app.use("/api/article",articleRouter);
-app.use("/api/user",userRouter);
+app.use("/api/auth",cors(corsParams),authRouter);
+app.use("/api/article",cors(corsParams),articleRouter);
+app.use("/api/user",cors(corsParams),userRouter);
 
 
 
