9fb85aee4d5f46a2968b0ac8e86b305af9da9ef8
Denys Novosad <denisnovosad3@gmail.com>
2023-08-19 11:05:44 +0300

    22

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const articleRouter = require("./routes/article.routes");
 const userRouter = require("./routes/user.roures");
 const cors = require("cors");
 const app = express();
-const corsParams = {
+/*const corsParams = {
   origin: ["https://deploy-my-front.vercel.app/"],
   methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
   credentials: true,
 };
-app.use(cors(corsParams));
+app.use(cors(corsParams));*/
 const PORT = /*config.get('SERVER_PORT')*/ process.env.SERVER_PORT || 5000;
 const DB_URL =
   process.env.DB_URL ||
   "mongodb+srv://varolou:AF9KvMedLj90iHGd@artd.nqpcmly.mongodb.net/?retryWrites=true&w=majority"; /*"mongodb+srv://varolou:5gRLitUDwPmSmOAv@cloud.upwzkiy.mongodb.net/?retryWrites=true&w=majority"*/
 
 app.use(express.json());
-app.use("/api/auth", authRouter);
-app.use("/api/article", articleRouter);
-app.use("/api/user", userRouter);
+app.use("/api/auth",cors(), authRouter);
+app.use("/api/article",cors(), articleRouter);
+app.use("/api/user",cors(), userRouter);
 
 const start = () => {
   try {
