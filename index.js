00fc56190845b6394c96640051886f940425aef6
Denys Novosad <denisnovosad3@gmail.com>
2023-08-19 10:40:02 +0300

    19

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const articleRouter = require("./routes/article.routes");
 const userRouter = require("./routes/user.roures");
 const cors = require("cors");
 const app = express();
-const corsParams = {
+/*const corsParams = {
   origin: ["https://deploy-my-front.vercel.app/"],
   methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
   credentials: true,
-};
+};*/
 //app.use(cors(corsParams));
 const PORT = /*config.get('SERVER_PORT')*/ process.env.SERVER_PORT || 5000;
 const DB_URL =
@@ -18,9 +18,9 @@ const DB_URL =
   "mongodb+srv://varolou:AF9KvMedLj90iHGd@artd.nqpcmly.mongodb.net/?retryWrites=true&w=majority"; /*"mongodb+srv://varolou:5gRLitUDwPmSmOAv@cloud.upwzkiy.mongodb.net/?retryWrites=true&w=majority"*/
 
 app.use(express.json());
-app.use("/api/auth", cors(corsParams), authRouter);
-app.use("/api/article", cors(corsParams), articleRouter);
-app.use("/api/user", cors(corsParams), userRouter);
+app.use("/api/auth", cors(), authRouter);
+app.use("/api/article", cors(), articleRouter);
+app.use("/api/user", cors(), userRouter);
 
 const start = () => {
   try {
